ed4f8f2ecd45d1530699bd53ffa237cb04b03b4d
Katie McTigue <kaitlinmctigue@gmail.com>
2021-11-03 02:59:45 -0400

    homepagr

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,21 @@
 import './Homepage.scss';
 import WindowBlock from './components/WindowBlock/WindowBlock';
+import BigLink from './components/BigLink/BigLink';
 import headshot from './images/headshot.png';
+import bandcamp from './images/bandcamp.png';
+import spotify from './images/spotify.png';
+import apple from './images/apple.png';
+import soundcloud from './images/soundcloud.png';
+import pandora from './images/pandora.png';
+import instagram from './images/instagram.png';
+import youtube from './images/youtube.png';
+import twitter from './images/twitter.png';
 
 function Homepage() {
     return (
         <div className="Homepage">
+
+            {/*Intro*/}
             <WindowBlock header={'Welcome!'}>
                 <div className={'intro'}>
                     <div className={'intro-pic'}>
@@ -29,6 +40,44 @@ function Homepage() {
                             </p>
                         </div>
                     </div>
+                </div>
+            </WindowBlock>
+
+            {/*Links*/}
+            <WindowBlock header={'Find My Music'} color={'blue'}>
+                <div className={'links'}>
+                    <BigLink name='bandcamp' img={bandcamp} link={'https://pacingmusic.bandcamp.com'} />
+                    <BigLink name='spotify' img={spotify} link={'https://open.spotify.com/artist/7mJOp9EcXEdAH6TL36flXw?si=CNLQNti2RWS1EOWVD17USQ'} />
+                    <BigLink name='apple music' img={apple} link={'https://music.apple.com/us/artist/pacing/1493615757'} />
+                    <BigLink name='soundcloud' img={soundcloud} link={'https://soundcloud.com/pacingmusic'} />
+                    <BigLink name='pandora' img={pandora} link={'https://www.pandora.com/playlist/PL:126724214806811588:342066879'}/>
+                </div>
+            </WindowBlock>
+
+            {/*MP3*/}
+            <WindowBlock header={'MP3 Player'} color={'green'}>
+                <div className={'links'}>
+                    <div dangerouslySetInnerHTML={{ __html: "<iframe style=\"border: 0; width: 200px; height: 314px;\" src=\"https://bandcamp.com/EmbeddedPlayer/track=986361956/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/\" seamless><a href=\"https://pacingmusic.bandcamp.com/track/im-getting-kicked-out-of-the-world\">I&#39;m Getting Kicked Out of the World by Pacing</a></iframe>"}} />
+                    <div dangerouslySetInnerHTML={{ __html: "<iframe style=\"border: 0; width: 200px; height: 342px;\" src=\"https://bandcamp.com/EmbeddedPlayer/album=3166585439/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/\" seamless><a href=\"https://pacingmusic.bandcamp.com/album/june-tinfoil-hat-song\">June / Tinfoil Hat Song by Pacing</a></iframe>"}} />
+                    <div dangerouslySetInnerHTML={{ __html: "<iframe style=\"border: 0; width: 200px; height: 314px;\" src=\"https://bandcamp.com/EmbeddedPlayer/track=2612382935/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/\" seamless><a href=\"https://pacingmusic.bandcamp.com/track/snow\">Snow by Pacing</a></iframe>"}} />
+                    {/*<div dangerouslySetInnerHTML={{ __html: "<iframe src='https://www.youtube.com/embed/cWDJoK8zw58' />"}} />*/}
+                </div>
+            </WindowBlock>
+
+            {/*Contact*/}
+            <WindowBlock header={'Contact Me'} color={'purple'}>
+                <div className={'text-box-large'}>
+                    pacingband@gmail.com
+                </div>
+            </WindowBlock>
+
+            {/*Social*/}
+            <WindowBlock header={'Be My Internet Friend'} color={'blue'}>
+                <div className={'links'}>
+                    <BigLink name='instagram' img={instagram} link={'https://www.instagram.com/pacingmusic/'} />
+                    <BigLink name='youtube' img={youtube} link={'https://www.youtube.com/channel/UCu9Dm575IZ4ICBZ-Bm6B9qw/featured'} />
+                    <BigLink name='twitter' img={twitter} link={'https://twitter.com/pacingmusic'} />
+
                 </div>
             </WindowBlock>
         </div>
--- src/components/WindowBlock/WindowBlock.js
+++ src/components/WindowBlock/WindowBlock.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './WindowBlock.scss';
+import icons from '../../images/windowicons.png';
 
 // const WindowBlock = ({children}) => (
 //   <div className="WindowBlock">
@@ -23,6 +24,7 @@ class WindowBlock extends Component {
                     <h1>
                         {this.props.header}
                     </h1>
+                    <img src={icons} className={'WindowBlock__header__icons'} />
                 </div>
                 <div className="WindowBlock__body">
                     {this.props.children}
