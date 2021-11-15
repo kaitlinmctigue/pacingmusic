5543c94657d17c6542f6f369ff882735fb76918f
Katie McTigue <kaitlinmctigue@gmail.com>
2021-11-15 12:58:31 -0500

    more desktop icons

--- src/components/DesktopDecor/DesktopDecor.js
+++ src/components/DesktopDecor/DesktopDecor.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types';
 import DesktopIcon from '../DesktopIcon/DesktopIcon';
 import './DesktopDecor.scss';
 import note from '../../images/note.png';
+import self from '../../images/self.png';
+import bird from '../../images/bird.png';
 
 const DesktopDecor = () => (
   <div className="DesktopDecor">
     <DesktopIcon idNum={1} icon={note} name={'1.txt'} />
+    <DesktopIcon idNum={2} img={self} name={'self.portrait'} />
+    <DesktopIcon idNum={3} img={bird} name={'thebigboss.jpeg'} />
   </div>
 );
 
